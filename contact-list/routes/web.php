d855471db77f99fb127fe7bf52b5f21af2b3df7e
Babak Chalaki <168085103+Babak-Chalacki@users.noreply.github.com>
2024-06-12 14:55:10 +0330

    Update web.php

--- contact-list/routes/web.php
+++ contact-list/routes/web.php
@@ -4,6 +4,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::get('/', [ContactController::class, 'index']);
+Route::get('/', [ContactController::class, 'index'])->name('home');
 Route::post('/new_contact', [ContactController::class, 'add']);
 Route::get('/remove', [ContactController::class, 'remove']);
